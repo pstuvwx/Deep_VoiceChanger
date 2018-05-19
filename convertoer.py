e32b4c9f6a6778e9ad0a04275d643d4b1a5a3659
如月和雅 <sptuvwx@outlook.jp>
2018-05-19 11:00:52 +0900

    Fix some miss

--- convertoer.py
+++ convertoer.py
@@ -15,6 +15,7 @@ from chainer.training import extensions
 from chainer.dataset import convert
 from chainer import cuda
 
+from joblib import Parallel, delayed
 import waver
 
 bps = 16000
@@ -23,7 +24,7 @@ wave_len = side*2-2
 wind = np.hanning(wave_len)
 test_len = 10
 data_size = 10000
-fft_resca = 15
+fft_resca = 18
 fft_scale = 1/fft_resca
 pow_scale = 0.2
     
@@ -65,6 +66,36 @@ class CBR(chainer.Chain):
             h = self.res(h)
         return h
             
+class Res(chainer.Chain):
+    def __init__(self, ch, bn=True, activation=F.leaky_relu, dropout=False, noise=False, res=None):
+        super(Res, self).__init__()
+        with self.init_scope():
+            self.bn = bn
+            self.activation = activation
+            self.noise = noise
+            self.dropout = dropout
+            self.res = res
+            self.c0 = L.Convolution2D(ch, ch, 3, 1, 1)
+            self.c1 = L.Convolution2D(ch, ch, 3, 1, 1)
+            if bn:
+                self.bn0 = L.BatchNormalization(ch)
+                self.bn1 = L.BatchNormalization(ch)
+
+    def __call__(self, x):
+        h = self.c0(x)
+        if self.bn:
+            h = self.bn0(h)
+        h = self.activation(h)
+        h = self.c1(x)
+        if self.bn:
+            h = self.bn1(h)
+        if self.dropout:
+            h = F.dropout(h)
+        h = self.activation(h)
+        if self.res is not None:
+            self.res(h)
+        return h + x
+
 gene_base = 64
 class Unet(chainer.Chain):
     def __init__(self):
@@ -90,20 +121,38 @@ class Unet(chainer.Chain):
         y  = self.d1(y, h1)
         return y
 
+class ResUnet(chainer.Chain):
+    def __init__(self):
+        super(ResUnet, self).__init__()
+        with self.init_scope():
+            self.c1=CBR(1,           gene_base*1, mode='down', res=Res(gene_base*1), bn=False)                       #128->64
+            self.c2=CBR(gene_base*1, gene_base*2, mode='down', res=Res(gene_base*2))                                 #64->32
+            self.c3=CBR(gene_base*2, gene_base*4, mode='down', res=Res(gene_base*4))                                 #32->16
+            self.c4=CBR(gene_base*4, gene_base*8, mode='down', res=Res(gene_base*8))                                 #16->8
+            self.d4=CBR(gene_base*8, gene_base*4, mode='up'  , res=Res(gene_base*4))                                 #8->16
+            self.d3=CBR(gene_base*8, gene_base*2, mode='up'  , res=Res(gene_base*2))                                 #16->32
+            self.d2=CBR(gene_base*4, gene_base*1, mode='up'  , res=Res(gene_base*1))                                 #32->64
+            self.d1=CBR(gene_base*2, 1,           mode='up'  , res=None,             activation=F.tanh,    bn=False) #64->128
+
+    def __call__(self, x):
+        h1 = self.c1(x)
+        h2 = self.c2(h1)
+        h3 = self.c3(h2)
+        y  = self.c4(h3)
+        y  = self.d4(y)
+        y  = self.d3(y, h3)
+        y  = self.d2(y, h2)
+        y  = self.d1(y, h1)
+        return y
+
 def load_comp(wave, num):
     images = [waver.image_single_split_pad(wave, side, i*side*(side-1)+wave_len, pow_scale, fft_scale, wind) for i in range(num)]
     return images
 
-def save_single(path, bps, src, side, power, scale):
-    if power is not None:
-        power = 1 / power
-    wave = waver.image_single_pad(src, side, power, scale, wind)
-    waver.save(path, bps, wave)
-
 def save_comp(path, bps, srces, side, power, scale):
     if power is not None:
         power = 1 / power
-    waves = [waver.image_single_pad(s, side, power, scale, wind) for s in srces]
+    waves = Parallel(n_jobs=8)([delayed(waver.image_single_pad)(s, side, power, scale, wind) for s in srces])
     wave = np.hstack(waves)    
     waver.save(path, bps, wave)
 
